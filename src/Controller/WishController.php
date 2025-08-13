31567255f3fe55557a9fea24b024746932b99c64
0oXiwiXo0 <christophe.rbrto@gmail.com>
2025-08-13 17:03:07 +0200

    TP Bucket-List

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -12,12 +12,32 @@ final class WishController extends AbstractController
     #[Route('/list', name: 'list')]
     public function list(): Response
     {
-        return $this->render('wish/list.html.twig');
+        $wishes = [
+            1 => 'Devenir riche Mother F****r !',
+            2 => 'Vendre des photos de pieds',
+            3 => 'Utiliser Ubuntu pour envoyer une fusée dans l\'espace'
+        ];
+
+        return $this->render('wish/list.html.twig', ['wishes' => $wishes]);
     }
 
     #[Route('/detail/{id}', name: 'detail')]
     public function detail(int $id): Response
     {
-        return $this->render('wish/detail.html.twig',['id' => $id]);
+
+        $wishesDetails = [
+            1 => 'Je veux devenir riche très vite pour vivre la meilleure vie possible.',
+            2 => 'Je veux lancer une boutique en ligne vendant des photos originales.',
+            3 => 'Je veux apprendre Ubuntu pour programmer une fusée spatiale.'
+        ];
+
+        if (!isset($wishesDetails[$id])) {
+            throw $this->createNotFoundException('Wish non trouvé.');
+        }
+
+        return $this->render('wish/detail.html.twig', [
+            'id' => $id,
+            'detailText' => $wishesDetails[$id],
+        ]);
     }
 }
