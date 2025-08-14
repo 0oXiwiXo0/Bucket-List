0ba7f97838b923339ee14c7574ad767b3c779b14
0oXiwiXo0 <christophe.rbrto@gmail.com>
2025-08-14 08:51:16 +0200

    fin du TP1 du module 05 - Données et Doctrine

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -13,30 +13,26 @@ final class WishController extends AbstractController
     #[Route('/list', name: 'list', methods: ['GET'])]
     public function list(WishRepository $wishRepository): Response
     {
-        $wishes = $wishRepository->findAll();
+        $wishes = $wishRepository->findBy([], ['dateCreated' => 'DESC']);
 
         return $this->render('wish/list.html.twig', [
             'wishes' => $wishes,
         ]);
     }
 
-    #[Route('/detail/{id}', name: 'detail')]
-    public function detail(int $id): Response
+    #[Route('/detail/{id}', name: 'detail', requirements: ['id' => '\d+'],methods: ['GET'] )]
+    public function detail(int $id, WishRepository $wishRepository): Response
     {
 
-        $wishesDetails = [
-            1 => 'Je veux devenir riche très vite pour vivre la meilleure vie possible.',
-            2 => 'Je veux lancer une boutique en ligne vendant des photos originales.',
-            3 => 'Je veux apprendre Ubuntu pour programmer une fusée spatiale.'
-        ];
+        $wishes = $wishRepository->find($id);
 
-        if (!isset($wishesDetails[$id])) {
-            throw $this->createNotFoundException('Wish non trouvé.');
+        if (!$wishes) {
+            throw $this->createNotFoundException('Wish non trouvé');
         }
 
-        return $this->render('wish/detail.html.twig', [
-            'id' => $id,
-            'detailText' => $wishesDetails[$id],
+        return $this->render('wish/detail.html.twig',[
+            'wishes' => $wishes,
         ]);
     }
 }
+
